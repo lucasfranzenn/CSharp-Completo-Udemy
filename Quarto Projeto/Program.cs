6e6257ee94bcdcdf5cc06f2f8a61d4a1197863c3
Lucas Franzen <lucasfranzen@alunos.utfpr.edu.br>
2024-04-28 22:31:31 -0300

    :sparkles: feat: Fourth Project

---
+++ Quarto Projeto/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Quarto_Projeto;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        InsereUsuario:
+
+        Console.Write("Digite o nome do usuário: ");
+        string? usuario = Console.ReadLine();
+
+        Console.Write("Digite a sua senha: ");
+        string? senha = "";
+
+        while (true)
+        {
+            ConsoleKeyInfo tecla = Console.ReadKey(true);
+            if(tecla is { Key: ConsoleKey.Enter })
+            {
+                Console.WriteLine("");
+                break;
+            }
+            else if(tecla is { Key: ConsoleKey.Backspace })
+            {
+                if (senha.Length == 0) { continue; }
+
+                senha = senha.Remove(senha.Length - 1);
+                Console.Write("\b \b");
+                continue;
+            }
+            else
+            {
+                Console.Write("*");
+                senha += tecla.KeyChar;
+            }
+        }
+
+        #region ValidaUsuario
+        if (usuario is not null && usuario.Equals("Lucas", StringComparison.OrdinalIgnoreCase) && senha.Equals("123"))
+        {
+            Console.WriteLine("Logado com sucesso!\nBem-vindo Lucas ♥");
+        }
+        else
+        {
+            Console.Write("Login incorreto!\nPressione qualquer tecla para tentar novamente");
+            Console.ReadKey(true);
+            Console.Clear();
+            goto InsereUsuario;
+        }
+        Console.Write("Pressione qualquer tecla para finalizar o programa!");
+        Console.ReadKey(true);
+        #endregion
+    }
+}
