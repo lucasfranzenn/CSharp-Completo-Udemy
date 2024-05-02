1463077e31d844600e63485f25ae16b3f43ebd11
Lucas Franzen <lucasfranzen@alunos.utfpr.edu.br>
2024-05-01 23:12:05 -0300

    :sparkles: feat: Eleventh Project

---
+++ DecimoPrimeiro Projeto/BaseDeDados.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace DecimoPrimeiro_Projeto;
+[DataContract]
+internal class BaseDeDados
+{
+    [DataMember]
+    private List<CadastroPessoa> _listaPessoas;
+    private string _caminhoBaseDeDados;
+
+    internal List<CadastroPessoa> ListaPessoas { get => _listaPessoas; set => _listaPessoas = value; }
+
+    public void AdicionarPessoa(CadastroPessoa pessoa)
+    {
+        ListaPessoas.Add(pessoa);
+        Serializador.Serializa(_caminhoBaseDeDados, this);
+    }
+
+    public List<CadastroPessoa> BuscarPessoa(string pDocumento)
+    {
+        List<CadastroPessoa> pessoasEncontradas = ListaPessoas.Where((x) => x.NumeroDocumento == pDocumento).ToList();
+
+        if (pessoasEncontradas.Count > 0)
+            return pessoasEncontradas;
+        else
+            return null;
+    }
+    public List<CadastroPessoa> ExcluirPessoa(string pDocumento)
+    {
+        List<CadastroPessoa> pessoasEncontradas = ListaPessoas.Where((x) => x.NumeroDocumento == pDocumento).ToList();
+
+        if (pessoasEncontradas.Count > 0)
+        {
+            foreach (CadastroPessoa pessoa in pessoasEncontradas)
+            {
+                ListaPessoas.Remove(pessoa);
+            }
+            Serializador.Serializa(_caminhoBaseDeDados, this);
+            return pessoasEncontradas;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public BaseDeDados(string caminhoBaseDeDados)
+    {
+        _caminhoBaseDeDados = caminhoBaseDeDados;
+        BaseDeDados temp = Serializador.Deserializa(caminhoBaseDeDados);
+        if (temp != null)
+        {
+            ListaPessoas = temp.ListaPessoas;
+        }
+        else
+        {
+            ListaPessoas = new();
+        }
+    }
+}
+
---
+++ DecimoPrimeiro Projeto/Serializador.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.IO;
+
+namespace DecimoPrimeiro_Projeto
+{
+    internal class Serializador
+    {
+        static private DataContractSerializer serializador = new DataContractSerializer(typeof(BaseDeDados));
+
+        public static void Serializa(string caminho, BaseDeDados dados)
+        {
+            XmlWriterSettings writerSettings = new XmlWriterSettings() { Indent = true };
+            StringBuilder stringBuilder = new();
+
+            using (XmlWriter writer = XmlWriter.Create(stringBuilder, writerSettings))
+            {
+                serializador.WriteObject(writer, dados);
+            }
+
+            string objetoSerializado = stringBuilder.ToString();
+
+            using (FileStream meuArquivoXML = File.Create(caminho));
+
+            File.WriteAllText(caminho, objetoSerializado);
+        }
+
+        public static BaseDeDados Deserializa(string caminho)
+        {
+            try
+            {
+                if (File.Exists(caminho))
+                {
+                    string conteudoObjeto = File.ReadAllText(caminho);
+
+                    StringReader stringReader  = new StringReader(conteudoObjeto);
+
+                    using (XmlReader xmlReader = XmlReader.Create(stringReader))
+                    {
+                        BaseDeDados temp = (BaseDeDados)serializador.ReadObject(xmlReader);
+                        return temp;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("EXCEÇÃO: " + e.Message);
+                return null;
+            }
+        }
+    }
+}
