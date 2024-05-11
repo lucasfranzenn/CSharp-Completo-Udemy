2ca9242c182a5a1d2f2923111f4eb33338f1d643
Lucas Franzen <lucasfranzen@alunos.utfpr.edu.br>
2024-05-11 16:31:03 -0300

    :sparkles: feat: Fourteenth Project

---
+++ DecimoQuarto Projeto/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DecimoQuarto_Projeto;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ DecimoQuarto Projeto/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DecimoQuarto_Projeto;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        mediaVideo.LoadedBehavior = MediaState.Manual;
+        sliderVolume.Value = 50;
+        mediaVideo.Volume = (sliderVolume.Value / 100);
+    }
+
+    private void sliderVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        mediaVideo.Volume = (sliderVolume.Value/100);
+    }
+
+    private void btnPlay_Click(object sender, RoutedEventArgs e)
+    {
+        mediaVideo.Play();
+    }
+
+    private void btnPause_Click(object sender, RoutedEventArgs e)
+    {
+        mediaVideo.Pause();
+    }
+
+    private void btnStop_Click(object sender, RoutedEventArgs e)
+    {
+        mediaVideo.Stop();
+    }
+
+    private void btnOpen_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog ofdVideo = new();
+        ofdVideo.InitialDirectory = @"c:\";
+        ofdVideo.Filter = "Mp4 video | *.mp4";
+
+        if(ofdVideo.ShowDialog() == true)
+        {
+            mediaVideo.Source = new Uri(ofdVideo.FileName);
+            mediaVideo.Play();
+        }
+    }
+}
